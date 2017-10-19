28e8bf57e18fc4b1c60bb46b8ac44a56fea68513
Jonathan Liu <jonathan.jy.liu@gmail.com>
2017-10-19 02:15:09 -0400

    added bash scripts to produce outputs

---
+++ decoder/run_as.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+file_name="decode_with_adjacent_swap.py"
+script_id="as"
+declare -a s_arr=(10000 20000 30000)
+declare -a k_arr=(10 20 30)
+
+for s in "${s_arr[@]}"
+do
+    for k in "${k_arr[@]}"
+    do
+        output_file="output_${script_id}s-${s}_k-${k}.txt"
+        python ${file_name} -s ${s} -k ${k} > ${output_file}
+        python compute-model-score < ${output_file}
+    done
+done
+
---
+++ decoder/run_rlfc_3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+file_name="decode_with_reordering_limit_future_cost.py"
+script_id="rlfc"
+declare -a s_arr=(5000)
+declare -a k_arr=(30)
+declare -a d_arr=(4 6 8 1000)
+
+for s in "${s_arr[@]}"
+do
+    for k in "${k_arr[@]}"
+    do
+        for d in "${d_arr[@]}"
+        do
+            output_file="output_${script_id}_s-${s}_k-${k}_d-${d}.txt"
+            python ${file_name} -s ${s} -k ${k} -d ${d} > ${output_file}
+            # python compute-model-score < ${output_file}
+        done
+    done
+done
+
